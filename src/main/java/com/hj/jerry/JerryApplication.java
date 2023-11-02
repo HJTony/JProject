dad34456f1a4416519fdefad74d214d4e3236818
tony <hyungjun.tony@gmail.com>
2023-11-02 10:04:18 +0900

    Comment: add todo list

--- src/main/java/com/hj/jerry/JerryApplication.java
+++ src/main/java/com/hj/jerry/JerryApplication.java
@@ -8,6 +8,13 @@ public class JerryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JerryApplication.class, args);
+		//TODO
+		/*
+		유저 인증, DB
+		광고 서비스
+		결재 서비스
+
+		 */
 	}
 
 }
