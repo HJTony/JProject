eb4bf186ea1847a97fdb63d1708e462dc22b5bff
tony <hyungjun.tony@gmail.com>
2023-11-07 22:01:57 +0900

    test: add memory repository test case

--- src/main/java/com/hj/jerry/repository/MemoryUserRepository.java
+++ src/main/java/com/hj/jerry/repository/MemoryUserRepository.java
@@ -1,2 +1,44 @@
-package com.hj.jerry.repository;public class MemoryUserRepository {
+package com.hj.jerry.repository;
+
+import com.hj.jerry.domain.User;
+
+import java.util.*;
+
+public class MemoryUserRepository implements UserRepositoy {
+
+    private static Map<Long, User> userMap = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(userMap.values());
+    }
+
+    @Override
+    public User save(User user) {
+        user.setId(++sequence);
+        userMap.put(sequence, user);
+        return user;
+
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.ofNullable(userMap.get(id));
+    }
+
+    @Override
+    public Optional<User> findByName(String name) {
+        return userMap.values().stream().filter(u -> u.getName().equals(name)).findAny();
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userMap.values().stream().filter(u -> u.getEmail().equals(email)).findAny();
+    }
+
+    public void clearStore() {
+        userMap.clear();
+        sequence=0;
+    }
 }
--- src/test/java/com/hj/jerry/repository/MemoryUserRepositoryTest.java
+++ src/test/java/com/hj/jerry/repository/MemoryUserRepositoryTest.java
@@ -1,4 +1,95 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.hj.jerry.repository;
+
+import com.hj.jerry.domain.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class MemoryUserRepositoryTest {
-  
+
+    MemoryUserRepository userRepositoy = new MemoryUserRepository();
+
+    @AfterEach
+    void afterEach() {
+        userRepositoy.clearStore();
+    }
+
+    @Test
+    void findAll() {
+        User u1 = new User();
+        User u2 = new User();
+        u1.setName("user1");
+        u2.setName("user2");
+
+        userRepositoy.save(u1);
+        userRepositoy.save(u2);
+
+        List<User> userList = userRepositoy.findAll();
+
+        boolean result = userList.containsAll(Arrays.asList(u1,u2));
+
+        assertTrue(result);
+    }
+
+    @Test
+    void save() {
+        User u1 = new User();
+        User u2 = new User();
+        u1.setName("user1");
+        u2.setName("user2");
+
+        userRepositoy.save(u1);
+        userRepositoy.save(u2);
+
+        List<User> userList = userRepositoy.findAll();
+
+        assertEquals(2, userList.size());
+    }
+
+    @Test
+    void findById() {
+        User u1 = new User();
+        String userName = "user1";
+        u1.setName(userName);
+
+        userRepositoy.save(u1);
+
+        User result = userRepositoy.findById(1L).get();
+
+        assertEquals(result.getId(), 1L);
+    }
+
+    @Test
+    void findByName() {
+        User u1 = new User();
+        String userName = "user1";
+        u1.setName(userName);
+
+        userRepositoy.save(u1);
+
+        User result = userRepositoy.findByName(userName).get();
+
+        assertEquals(result.getName(), userName);
+    }
+
+    @Test
+    void findByEmail() {
+        User u1 = new User();
+        String userName = "user1";
+        String email = "user1@gmail.com";
+        u1.setName(userName);
+        u1.setEmail(email);
+
+        userRepositoy.save(u1);
+
+        User result = userRepositoy.findByEmail(email).get();
+
+        assertEquals(result.getEmail(), email);
+    }
+
 }
